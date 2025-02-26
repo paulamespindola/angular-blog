1adb89d13b98085dca8bea77ed4e02b1f9323de4
paulamespindola <paulamespindola04@gmail.com>
2025-02-26 16:28:53 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Novidades da nova versão do Angular",
+    "description": "Muitas funcionalidades extras",
+    "photoCover": "https://ionic.io/blog/wp-content/uploads/2024/02/angular-feature-image-1-1024x512.png"
+  },
+  {
+    "id":"2",
+    "title": "Dicas para o uso de Node.js",
+    "description": "Instale o node.js e embarque nessa aventura conosco!",
+    "photoCover": "https://media.licdn.com/dms/image/v2/D4D12AQFz_qGIWZzAcQ/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1682940169384?e=2147483647&v=beta&t=-mo5RFr0YqRh5vQippLzQFKVGyaXL94VVJ6NlOSEoTE"
+  }
+]
